623eff3cece4a75a51a2978293f3b032ecd28dc8
tomlloydDEV <tom11b@hotmail.co.uk>
2026-03-11 16:17:59 +0000

    set dataclasses in models.py

---
+++ src/fc26/domain/models.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass 
+from datetime import datetime 
+
+@dataclass
+class SalePrint:
+    asset_id: str
+    platform: str
+    ts_utc: datetime 
+    price_gross: int
+
+
+@dataclass
+class Bar:
+    asset_id: str
+    platform: str
+    timeframe: str
+    windodw_start_utc: datetime 
+    open: int
+    high: int
+    low: int
+    close: int
+    volume: int
+
